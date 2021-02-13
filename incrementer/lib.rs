23fb0b43388c40801ed27d46e23aa05d15b2ee38
Sokos6 <will.sokolowski@gmail.com>
2021-02-12 23:42:28 -0600

    lib refactor

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -11,39 +11,22 @@ mod incrementer {
     #[ink(storage)]
     pub struct Incrementer {
         /// Stores a single `bool` value on the storage.
-        value: bool,
-        value2: i32,
+        value: i32,
     }
 
     impl Incrementer {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
         #[ink(constructor)]
-        pub fn new(init_value: bool, init_value2: i32) -> Self {
-            Self {
-                value: init_value,
-                value2: init_value2,
-            }
+        pub fn new(init_value: i32) -> Self {
+            Self { value: init_value }
         }
 
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self::new(Default::default(), Default::default())
+            Self { value: 0 }
         }
 
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
         #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
+        pub fn get(&self) -> i32 {
             self.value
         }
     }
@@ -60,16 +43,7 @@ mod incrementer {
         #[test]
         fn default_works() {
             let incrementer = Incrementer::default();
-            assert_eq!(incrementer.get(), false);
-        }
-
-        /// We test a simple use case of our contract.
-        #[test]
-        fn it_works() {
-            let mut incrementer = Incrementer::new(false, 1);
-            assert_eq!(incrementer.get(), false);
-            incrementer.flip();
-            assert_eq!(incrementer.get(), true);
+            assert_eq!(incrementer.get(), 0);
         }
     }
 }
