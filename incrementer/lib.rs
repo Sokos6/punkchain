7465d9636f8fe684b6acb8ab230ce29d71cee486
Sokos6 <will.sokolowski@gmail.com>
2021-02-13 00:23:37 -0600

    hashmappy and inc complete

--- incrementer/lib.rs
+++ incrementer/lib.rs
@@ -4,46 +4,87 @@ use ink_lang as ink;
 
 #[ink::contract]
 mod incrementer {
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
     #[ink(storage)]
     pub struct Incrementer {
-        /// Stores a single `bool` value on the storage.
         value: i32,
+        my_value: ink_storage::collections::HashMap<AccountId, i32>,
     }
 
     impl Incrementer {
         #[ink(constructor)]
         pub fn new(init_value: i32) -> Self {
-            Self { value: init_value }
+            Self {
+                value: init_value,
+                my_value: ink_storage::collections::HashMap::new(),
+            }
         }
 
         #[ink(constructor)]
         pub fn default() -> Self {
-            Self { value: 0 }
+            Self {
+                value: 0,
+                my_value: Default::default(),
+            }
         }
 
         #[ink(message)]
         pub fn get(&self) -> i32 {
             self.value
         }
+
+        #[ink(message)]
+        pub fn inc(&mut self, by: i32) {
+            self.value += by;
+        }
+
+        #[ink(message)]
+        pub fn get_mine(&self) -> i32 {
+            self.my_value_or_zero(&self.env().caller())
+        }
+
+        #[ink(message)]
+        pub fn inc_mine(&mut self, by: i32) {
+            let caller = self.env().caller();
+            let my_value = self.my_value_or_zero(&caller);
+            self.my_value.insert(caller, my_value + by);
+        }
+
+        fn my_value_or_zero(&self, of: &AccountId) -> i32 {
+            *self.my_value.get(of).unwrap_or(&0)
+        }
     }
 
-    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
-    /// module and test functions are marked with a `#[test]` attribute.
-    /// The below code is technically just normal Rust code.
     #[cfg(test)]
     mod tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
+        use ink_lang as ink;
+
         #[test]
         fn default_works() {
-            let incrementer = Incrementer::default();
-            assert_eq!(incrementer.get(), 0);
+            let contract = Incrementer::default();
+            assert_eq!(contract.get(), 0);
+        }
+
+        #[test]
+        fn it_works() {
+            let mut contract = Incrementer::new(42);
+            assert_eq!(contract.get(), 42);
+            contract.inc(5);
+            assert_eq!(contract.get(), 47);
+            contract.inc(-50);
+            assert_eq!(contract.get(), -3);
+        }
+
+        #[ink::test]
+        fn my_value_works() {
+            let mut contract = Incrementer::new(11);
+            assert_eq!(contract.get(), 11);
+            assert_eq!(contract.get_mine(), 0);
+            contract.inc_mine(5);
+            assert_eq!(contract.get_mine(), 5);
+            contract.inc_mine(10);
+            assert_eq!(contract.get_mine(), 15);
         }
     }
 }
